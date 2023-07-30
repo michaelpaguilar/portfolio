93d10fc06cb972594f96cd73e53b4791b3bb759d
Michael Aguilar <Michael.P.Aguilar@gmail.com>
2023-07-29 21:54:06 -0700

    Updated Icons and Resume

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,10 +3,10 @@ module.exports = {
     {
       resolve: "gatsby-theme-portfolio-minimal",
       options: {
-        siteUrl: "https://gatsby-starter-portfolio-minimal-theme.netlify.app/", // Used for sitemap generation
+        siteUrl: "https://michaelpaguilar.netlify.app/", // Used for sitemap generation
         manifestSettings: {
           favicon: "./content/images/notebook.png", // Path is relative to the root
-          siteName: "My Minimal Portfolio", // Used in manifest.json
+          siteName: "Michael Aguilar Portfolio", // Used in manifest.json
           shortName: "Portfolio", // Used in manifest.json
           startUrl: "/", // Used in manifest.json
           backgroundColor: "#FFFFFF", // Used in manifest.json
